86f5bfa208950584b4ab77f179bd1e432c807c00
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2023-01-17 22:06:09 +0800

    Add project files.

---
+++ Telegram-Bulk-Sender-Invite-Extract-Marketing-Pro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram_Bulk_Sender_Invite_Extract_Marketing_Pro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
